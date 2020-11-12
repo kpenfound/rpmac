cefa1ca3381f5596141615f0986ffa44bc1c9dfe
Kyle Penfound <kpenfound11@gmail.com>
2020-11-12 14:36:37 -0500

    Install multiple packages with formatting

--- cli/install.go
+++ cli/install.go
@@ -2,7 +2,9 @@ package cli
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/jedib0t/go-pretty/table"
 	"github.com/kpenfound/rpmac/repository"
 )
 
@@ -32,24 +34,47 @@ func (i *InstallCommand) Run(args []string) int {
 		return 1
 	}
 
-	rpm, err := r.Query(args[0])
-	if err != nil {
-		fmt.Printf("Error querying for package: %s\n", err)
-		return 1
-	}
-	fmt.Printf("Installing %s/%s\n", rpm.Repository.ID, rpm.Package.Name)
+	toInstall := []*repository.RepoPackage{}
 
-	err = rpm.Package.Install(rpm.Repository.BaseURL)
-	if err != nil {
-		fmt.Printf("Error installing package: %s\n", err)
-		return 1
+	for _, a := range args {
+		pack, err := r.Query(a)
+		if err != nil {
+			fmt.Printf("Error querying for package: %s\n", err)
+			return 1
+		}
+		toInstall = append(toInstall, pack)
 	}
-	fmt.Printf("Installed %s\n", rpm.Package.Name)
-	err = rpm.Repository.Save()
-	if err != nil {
-		fmt.Printf("Error saving package state: %s\n", err)
-		return 1
+
+	fmt.Printf("Resolved Dependnecies...\n\n")
+
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	style := t.Style()
+	style.Options = table.OptionsNoBordersAndSeparators
+	t.SetStyle(*style)
+
+	t.AppendHeader(table.Row{"Package", "Version", "Repository", "Size"})
+	for _, i := range toInstall {
+		t.AppendRow(table.Row{i.Package.Name, i.Package.Version.Version, i.Repository.Name, i.Package.Size.Installed})
 	}
+	t.Render()
+	fmt.Printf("\n")
+
+	// TODO: confirm install
+	for _, rpm := range toInstall {
+		err = rpm.Package.Install(rpm.Repository.BaseURL)
+		if err != nil {
+			fmt.Printf("Error installing package: %s\n", err)
+			return 1
+		}
+		fmt.Printf("Installed %s\n", rpm.Package.Name)
+		err = rpm.Repository.Save()
+		if err != nil {
+			fmt.Printf("Error saving package state: %s\n", err)
+			return 1
+		}
+	}
+	fmt.Println("Installation complete!")
 
 	return 0
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,9 @@ go 1.15
 require (
 	github.com/cavaliercoder/badio v0.0.0-20160213150051-ce5280129e9e // indirect
 	github.com/cavaliercoder/go-rpm v0.0.0-20200122174316-8cb9fd9c31a8
+	github.com/go-openapi/strfmt v0.19.8 // indirect
+	github.com/jedib0t/go-pretty v4.3.0+incompatible
+	github.com/mattn/go-runewidth v0.0.9 // indirect
 	github.com/mitchellh/cli v1.1.2
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/sassoftware/go-rpmutils v0.1.1
